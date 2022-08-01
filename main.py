70a42bf11c035b6244b4a28e414632dd83609ad6
Usman <unumer@hotmail.com>
2022-08-01 13:26:30 +0100

    changed tv switchport to vlan 20 (iot)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from net_automation import net_automation
 
+net_automation.Cisco_IOS.deploy_yaml("2960-usman-lan.yml")
 net_automation.Vyos.deploy_yaml("edge-dn42-lan.yml")
 net_automation.Vyos.deploy_yaml("edge-ca-dn42-lan.yml")
