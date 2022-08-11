2da9fdb36e3e9d89804ce3cac99e7c62110c89ee
Usman <unumer@hotmail.com>
2022-08-11 14:09:12 +0100

    added descs to 296-usman-lan, moved configs into folder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from net_automation import net_automation
 
-# net_automation.Cisco_IOS.deploy_yaml_now("2960-usman-lan.yml")
-net_automation.Vyos.deploy_yaml_now("edge-dn42-lan.yml")
+net_automation.Cisco_IOS.deploy_yaml_now("configs/2960-usman-lan.yml")
+# net_automation.Vyos.deploy_yaml_now("configs/edge-dn42-lan.yml")
 # net_automation.Vyos.deploy_yaml("edge-ca-dn42-lan.yml")
