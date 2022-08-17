b81d323cce2fc9ba42f56e91a218f4c4d7e30ed9
Zahid <78828685+F2ncy@users.noreply.github.com>
2022-08-17 17:42:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from net_automation import net_automation
 
+net_automation.EdgeOS.deploy_yaml_now("configs/erx-zahid-lan.yml")
 net_automation.EdgeOS.deploy_yaml_now("configs/erx-usman-lan.yml")
 #net_automation.Cisco_IOS.deploy_yaml_now("configs/2960-usman-lan.yml")
 # net_automation.Vyos.deploy_yaml_now("configs/edge-dn42-lan.yml")
