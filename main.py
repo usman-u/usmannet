d63c690ff9ad5755d650073ecbd43cfc1f60615d
Usman <unumer@hotmail.com>
2022-09-11 18:25:37 +0100

    added lutoma BGP peer, also added diagnostics to main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from net_automation import net_automation
 # net_automation.EdgeOS.deploy_yaml_now("configs/erx-usman-lan.yml")
 net_automation.Vyos.deploy_yaml_now("configs/edge-dn42-lan.yml")
 # net_automation.Vyos.deploy_yaml("edge-ca-dn42-lan.yml")
+
+print ("Diagnostics:")
+edge = net_automation.Vyos("edge.dn42.lan", "test", "test", False, "", "")
+print (edge.init_ssh())
+print (edge.get_bgp_neighbors())
+print (edge.get_interfaces())
