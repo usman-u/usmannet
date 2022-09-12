01b3c048a2d1617c6edb305f1012a6e0ea21a9a7
Usman <unumer@hotmail.com>
2022-09-12 22:20:34 +0100

    fixed auth parms

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ net_automation.Vyos.deploy_yaml("configs/edge-dn42-lan.yml")
 print ("Diagnostics:")
 edge = net_automation.Vyos(
     "edge.dn42.lan",
-    "usman",
+    "test",
+    "test",
+    False,
     "",
-    True,
-    r"c:\Users\Usman\.ssh\id_rsa",
 )
 
 edge = net_automation.Vyos()
