2636cc16ea1399cb962585d3fa388ea1d87172fb
Usman <unumer@hotmail.com>
2022-09-14 16:07:09 +0100

    DN42: enable kioubit (bgp + wg13? 2960: change g0/5 int desc to rasp pi

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from net_automation import net_automation
 
 # net_automation.EdgeOS.deploy_yaml("configs/erx-zahid-lan.yml")
-# net_automation.Cisco_IOS.deploy_yaml("configs/2960-usman-lan.yml")
+net_automation.Cisco_IOS.deploy_yaml("configs/2960-usman-lan.yml")
 # net_automation.EdgeOS.deploy_yaml("configs/erx-usman-lan.yml")
 net_automation.Vyos.deploy_yaml("configs/edge-dn42-lan.yml")
 # net_automation.Vyos.deploy_yaml("edge-ca-dn42-lan.yml")
 
 print ("Diagnostics:")
 edge = net_automation.Vyos(
-    "vyos",
-    "edge.dn42.lan",
-    "test",
-    "test",
-    False,
-    "",
+    device_type = "vyos",
+    host = "edge.dn42.lan",
+    username = "test",
+    password = "test",
 )
 
 print (edge.init_ssh())
