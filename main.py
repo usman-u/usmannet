dde5b4eee874536cf0a43cf2a0390b627501dc0b
Usman <unumer@hotmail.com>
2022-10-14 16:26:22 +0100

    c2960.usman: set g0/6 to vlan10 (server), update main.py with swport verify

--- main.py
+++ main.py
@@ -7,21 +7,38 @@ net_automation.Cisco_IOS.deploy_yaml("configs/2960-usman-lan.yml")
 # net_automation.Vyos.deploy_yaml("configs/edge-dn42-lan.yml")
 # net_automation.Vyos.deploy_yaml("edge-ca-dn42-lan.yml")
 
-# print ("Diagnostics:")
-# edge = net_automation.Vyos(
-#     device_type = "vyos",
-#     host = "edge.dn42.lan",
-#     username = "test",
-#     password = "test",
-# )
-
-# print (edge.init_ssh())
-# print ("----------------- Interfaces -----------------")
-# print (edge.get_interfaces())
-# time.sleep(15)
-# print ("----------------- BGP Neighb -----------------")
+def verify_cicso_switchports():
+    ciscosw = net_automation.Cisco_IOS(
+        device_type = "cisco_ios",
+        host = "2960-usman-lan",
+        username = "test",
+        password = "test",
+    )
+    ciscosw.init_ssh()
+    print (ciscosw.custom_command("sh vlan"))
+    print (ciscosw.custom_command("sh ip int desc"))
 
 
-# print (edge.custom_command("sudo wg"))
 
-# print (edge.run_ping("172.23.0.53", 5))
+def verify_dn42_peerings():
+    print ("Diagnostics:")
+    edge = net_automation.Vyos(
+        device_type = "vyos",
+        host = "edge.dn42.lan",
+        username = "test",
+        password = "test",
+    )
+
+    print (edge.init_ssh())
+    print ("----------------- Interfaces -----------------")
+    print (edge.get_interfaces())
+    time.sleep(15)
+    print ("----------------- BGP Neighb -----------------")
+
+
+    print (edge.custom_command("sudo wg"))
+
+    print (edge.run_ping("172.23.0.53", 5))
+
+
+verify_cicso_switchports()
