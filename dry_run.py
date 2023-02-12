dcd6160c4466403efde5325dcccbcfa3f6742401
Usman <unumer@hotmail.com>
2023-02-12 22:53:11 +0000

    fr-lil1: rename yaml, delete lutoma fw rule, delete old prefix lists, add descs

--- dry_run.py
+++ dry_run.py
@@ -1,3 +1,3 @@
 from net_automation import net_automation
 
-net_automation.Vyos.deploy_yaml("configs/edge-dn42-lan.yml", True)
+net_automation.Vyos.deploy_yaml("configs/fr-lil1-usman-dn42.yml", True)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 # net_automation.EdgeOS.deploy_yaml("configs/erx-zahid-lan.yml")
 # net_automation.Cisco_IOS.deploy_yaml("configs/2960-usman-lan.yml")
 # net_automation.EdgeOS.deploy_yaml("configs/erx-usman-lan.yml")
-net_automation.Vyos.deploy_yaml("configs/edge-dn42-lan.yml", dry_run=False)
+net_automation.Vyos.deploy_yaml("configs/fr-lil1-usman-dn42.yml", dry_run=False)
 # net_automation.Vyos.deploy_yaml("edge-ca-dn42-lan.yml")
 
 # def verify_cicso_switchports():
